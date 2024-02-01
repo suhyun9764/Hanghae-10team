d74a3a9fe85e86efb1a468e06b042e633689c5c6
suhyun9764 <suhyun9764@naver.com>
2024-02-01 13:05:12 +0900

    create db

--- app.py
+++ app.py
@@ -1,8 +1,52 @@
 from flask import Flask, render_template, request
-import requests
+import requests,os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
 
+db = SQLAlchemy(app)
+
+class Member(db.Model):
+    member_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pwd = db.Column(db.String, nullable=False)
+    memos = relationship('Memo', backref='member', lazy=True)
+    todo_lists = relationship('ToDoList', backref='member', lazy=True)
+    address_books = relationship('AddressBook', backref='member', lazy=True)
+    diaries = relationship('Diary', backref='member', lazy=True)
+
+class Memo(db.Model):
+    memo_id = db.Column(db.Integer, primary_key=True)
+    main_text = db.Column(db.Text, nullable=False)
+    edit_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class ToDoList(db.Model):
+    todolist_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    deadline = db.Column(db.Date, nullable=True)
+    is_complete = db.Column(db.Boolean, nullable=False, default=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class AddressBook(db.Model):
+    address_id = db.Column(db.Integer, primary_key=True)
+    team_member_name = db.Column(db.String, nullable=False)
+    project = db.Column(db.String, nullable=False)
+    realization = db.Column(db.Text, nullable=True)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
+
+class Diary(db.Model):
+    diary_id = db.Column(db.Integer, primary_key=True)
+    main_text = db.Column(db.Text, nullable=False)
+    edit_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    member_id = db.Column(db.String, db.ForeignKey('member.member_id'), nullable=False)
 
 @app.route("/memo/")
 def memo():
